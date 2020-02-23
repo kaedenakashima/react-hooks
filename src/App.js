cd2710946e97d6ad754dd923dec3408dd530e81f
Katie <Katie@Sandstone.local>
2020-02-23 13:34:36 +0900

    save before make a branch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  createContext
+} from 'react';
+import Counter from './Counter';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
+  const reverseWord = word => {
+    console.log('function called');
+    return word
+      .split('')
+      .reverse()
+      .join('');
+  };
+
+  const TitleReversed = reverseWord('Level Up Dishes');
+
   return (
-    <div className='main-wrapper' ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
-        Level Up Dishes
-      </h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          type='text'
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+      <div className='main-wrapper' ref={ref}>
+        <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
+          {TitleReversed}
+        </h1>
+        <Toggle />
+        <Counter />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type='text'
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
