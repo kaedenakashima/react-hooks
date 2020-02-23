e90d2715c43989acfbc76c47c7e85fdd732d47d3
Katie <Katie@Sandstone.local>
2020-02-23 12:58:09 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Toggle from './Toggle';
+import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
+  const [name, setName] = useTitleInput('');
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className='main-wrapper' ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
+        Level Up Dishes
+      </h1>
+      <Toggle />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type='text'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log('email sent to' + value + '!');
+  setValue('');
 };
 
 export default App;
